c94ec88596bab5b26412f6750025db4face7e947
Anders Farr <35501990+16ajjf@users.noreply.github.com>
2023-10-01 14:14:20 -0700

    Created Django Rest API from Django Rest Tutoria

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+urlpatters = format_suffix_patterns(urlpatterns)
